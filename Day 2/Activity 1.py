c7a5a4907ac8d0d1d126113f30b3ec7a2a34da82
Sreethumohan <122439184+Sreethumohan@users.noreply.github.com>
2024-05-17 14:16:57 +0530

    Add files via upload

---
+++ Day 2/Activity 1.py
@@ -0,0 +1,4 @@
+# Remove an element from a list by value.
+list = [1,2,3,4,5]
+list.remove(2)
+print(list)
---
+++ Day 2/Activity 2.py
@@ -0,0 +1,5 @@
+# Convert a tuple to a list
+
+tuple = (1, 2, 3, 4, 5)
+list = list(tuple)
+print(list)
